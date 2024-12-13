cac556b97048087304370e7d34bb6c2088cbf92c
Douglas <douglascomercial03@gmail.com>
2024-12-13 15:19:35 -0300

    Additing Json response format in console

--- api/src/main/java/java_crud/Main.java
+++ api/src/main/java/java_crud/Main.java
@@ -2,6 +2,9 @@ package java_crud;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import java_crud.dto.EnderecoDTO;
 import java_crud.serviço.ApiServico;
 
@@ -11,8 +14,13 @@ public class Main {
         ApiServico apiServico = new ApiServico();
 
         try {
-            EnderecoDTO enderecoDTO = apiServico.getEndereco("88330340");
-            System.out.println(enderecoDTO);
+            EnderecoDTO enderecoDTO = apiServico.getEndereco("01153000");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            String json = objectMapper.writeValueAsString(enderecoDTO);
+            System.out.println(json);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
